c4aafa4a164c25a80d5df31fca1740305c6f2d63
Camael Ventura <camael.ventura@gmail.com>
2023-03-19 19:15:50 -0300

    showing user profile

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,13 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const databaseUser = this.usersRepository.findById(user_id);
+
+    if (!databaseUser) {
+      throw new Error("User not found!");
+    }
+
+    return databaseUser;
   }
 }
 
